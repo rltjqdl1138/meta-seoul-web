e5822f38fe23cba095c49fdfdf8170344630cdf8
Kim ki seop <rltjqdl1138@naver.com>
2022-02-18 14:16:03 +0900

    temp

--- src/Component/MapComponent.js
+++ src/Component/MapComponent.js
@@ -11,8 +11,123 @@ function MapComponent({onSelect, setProps, total}) {
     const [zoom, setZoom] = useState(15);
     let hoveredStateId = null;
     
-    let hoveredCell = {}
+    let loadedArea = [
+        [
+            '2A187453064356c898cAe034EAed119E1663ACb8',
+            '2953399124f0cbb46d2cbacd8a89cf0599974963',
+            '31d4C5be1082A88F2ABAFeA549B6C189C2cf057F',
+        ],
+        [
+            'F87E31492Faf9A91B02Ee0dEAAd50d51d56D5d4d',
+            'BD4455dA5929D5639EE098ABFaa3241e9ae111Af',
+            'd2F668a8461D6761115dAF8Aeb3cDf5F40C532C6',
+        ],
+        [   
+            '79986aF15539de2db9A5086382daEdA917A9CF0C',
+            '495f947276749Ce646f68AC8c248420045cb7b5e',
+            '6e4c6D9B0930073e958ABd2ABa516b885260b8Ff',
+            
+        ],
+    ]
+    let lastCell = null
+    let hoveredCell = null
     let selectedCell = null
+    const moveEvent = (currentCell)=>{
+        const currentArea = findArea(currentCell.source)
+        const lastArea = findArea(lastCell.contract_id)
+        const firstArea = findArea(selectedCell.source)
+
+        const startX = firstArea.x < lastArea.x ? firstArea.x : lastArea.x
+        const endX = firstArea.x > lastArea.x ? firstArea.x : lastArea.x
+        const startY = firstArea.y < lastArea.y ? firstArea.y : lastArea.y
+        const endY = firstArea.y > lastArea.y ? firstArea.y : lastArea.y
+
+        let isEffectX = lastCell.x === currentCell.properties.x ? false : true
+        let isEffectY = lastCell.y === currentCell.properties.y ? false : true
+
+        // 셀 진행 방향
+        // 셀 
+        if(isEffectX)
+            for(let j=startY; j<=endY; j++)
+                don(loadedArea[j][currentArea.x], currentCell, 'x')
+        if(isEffectY)
+            for(let i=startX; i<=endX; i++)
+                don(loadedArea[currentArea.y][i], currentCell, 'y')
+    }
+    const don = (contract_id, currentCell, type)=>{
+        const me = contract_id === currentCell.source ? true : false
+        if(contract_id === selectedCell.source);
+
+        const firstArea = findArea(selectedCell.source)
+        const currentArea = findArea(contract_id)
+        const lastArea = findArea(lastCell.contract_id)
+
+        let {x:startX,y:startY} = selectedCell.properties
+        const { x:endX,  y:endY } = currentCell.properties
+        const {x:lastX, y:lastY } = lastCell
+        if(type === 'x'){
+            const AreaDirection = currentArea.x - firstArea.x
+            const CellDirection = AreaDirection === 0 ? endX - startX : AreaDirection
+            const hoverDirection = currentArea.x - lastArea.x || endX - lastX
+            const isExtendedX = CellDirection * hoverDirection > 0
+            const isOverArea = lastArea.x - currentArea.x
+            const _end_x = isExtendedX ? endX : hoverDirection > 0 ? endX-1 : endX+1
+            const _start_x = isOverArea!==0 ? (hoverDirection>0 ? 0 : 99) : (isExtendedX ? (hoverDirection>0 ? lastX+1 : lastX-1) : lastX)
+            const end_x = _end_x > _start_x ? _end_x : _start_x
+            const start_x = _end_x < _start_x ? _end_x : _start_x
+            const _start_y = firstArea.y === currentArea.y ? selectedCell.properties.y : 0
+            const _end_y = me ? (isExtendedX ? endY : lastY) : 99
+
+            const start_y = _end_y > _start_y ? _start_y : _end_y
+            const end_y = _end_y < _start_y ? _start_y : _end_y
+
+            
+            console.log(firstArea)
+            console.log(currentArea)
+            console.log(lastArea)
+            console.log({CellDirection, hoverDirection, isExtendedX,isOverArea,})
+            console.log(`X:${start_x}~${end_x} Y:${startY}~${endY}`)
+            for(let i=start_x; i<=end_x; i++)
+                for(let j=start_y; j<=end_y; j++)
+                    map.current.setFeatureState( { source: contract_id, id: i*100 + j },{ hover: isExtendedX } );
+
+            
+        }
+        if(type === 'y'){
+            const AreaDirection = currentArea.y - firstArea.y
+            const CellDirection = AreaDirection === 0 ? endY - startY : AreaDirection
+            const hoverDirection = currentArea.y - lastArea.y || endY - lastY
+            const isExtendedY = CellDirection * hoverDirection > 0
+            const isOverArea = lastArea.y - currentArea.y
+            const _end_y = isExtendedY ? endY : hoverDirection > 0 ? endY-1 : endY+1
+            const _start_y = isOverArea!==0 ? (hoverDirection>0 ? 0 : 99) : (isExtendedY ? (hoverDirection>0 ? lastY+1 : lastY-1) : lastY)
+            const end_y = _end_y > _start_y ? _end_y : _start_y
+            const start_y = _end_y < _start_y ? _end_y : _start_y
+            const _start_x = firstArea.x === currentArea.x ? selectedCell.properties.x : 0
+            const _end_x = me ? (isExtendedY ? endX : lastX) : 99
+
+            const start_x= _end_x > _start_x ? _start_x : _end_x
+            const end_x = _end_x < _start_x ? _start_x : _end_x
+            
+            
+            //
+            for(let x = start_x; x <= end_x; x++)
+                for(let y = start_y; y <= end_y; y++)
+                    map.current.setFeatureState( { source: contract_id, id: x*100 + y },{ hover: isExtendedY } );
+
+        }
+        if(type === 'me'){
+            console.log('me')
+        }
+    }
+    const findArea = (contract_id)=>{
+        for(let i=0; i<3; i++){
+            for(let j=0; j<3; j++)
+                if(loadedArea[j][i] === contract_id)
+                    return {x:i, y:j}
+        }
+    }
+
     const load = (contract_id)=>{
         map.current.on('wheel', e=>{
             setProps('zoom', map.current.getZoom().toFixed(4))
@@ -28,12 +143,12 @@ function MapComponent({onSelect, setProps, total}) {
             source:     contract_id,
             layout:     {},
             paint:      {
-                'fill-color': '#627BC1',
+                'fill-color': '#000000',//'#627BC1',
                 'fill-opacity': [
                     'case',
                     ['boolean', ['feature-state', 'hover'], false],
-                    0.9,
-                    0.2
+                    1.0,
+                    0.0
                 ]
             }
         });
@@ -90,19 +205,24 @@ function MapComponent({onSelect, setProps, total}) {
 
         map.current.on('mousemove', contract_id+'-fills', e => {
             if (e.features.length > 0) {
-                if(!selectedCell){
-                    if (hoveredStateId !== null)
-                        map.current.setFeatureState( { source: contract_id, id: hoveredStateId }, { hover: false } );
-                    hoveredStateId = e.features[0].id;
-                    map.current.setFeatureState( { source: contract_id, id: hoveredStateId },{ hover: true } );
-                }else{
-                    if( selectedCell.source !== contract_id ) {
-                        return
-                    }else{
-                        console.log('go')
-                    }
+                const currentCell = e.features[0]
 
-                    const {x:startX,y:startY} = selectedCell.properties
+                if(lastCell === null);
+                else if(currentCell.id === lastCell.id && currentCell.source === lastCell.contract_id)
+                    return;
+                
+                if(!selectedCell){
+                    if(lastCell !== null)
+                        map.current.setFeatureState( { source: lastCell.contract_id, id: lastCell.id }, { hover: false } );
+                    
+                    map.current.setFeatureState( { source: contract_id, id: currentCell.id },{ hover: true } );
+                }else{
+                    
+                    moveEvent(currentCell)
+                    /*
+                    let {x:startX,y:startY} = selectedCell.properties
+                    if(selectedCell.source !== currentCell.source)
+                        startX=0
                     const { x:endX,  y:endY } = e.features[0].properties
                     const {x:lastX, y:lastY } = hoveredCell.properties
                     const isExtendedX = Math.abs(startX - endX) - Math.abs(startX - lastX) > 0 ? true : false
@@ -110,7 +230,7 @@ function MapComponent({onSelect, setProps, total}) {
                     const totalSelected = (Math.abs(startX-endX)+1)*(Math.abs(startY-endY)+1)
                     setProps('tempCount', totalSelected)
 
-                    if (hoveredStateId !== null){
+                    if (lastCell !== null){
                         if(endX !== lastX && endY !== lastY){
                             if( !isExtendedX && !isExtendedY )
                                 map.current.setFeatureState( { source: contract_id, id: lastX*100 + lastY },{ hover: false } );
@@ -150,8 +270,9 @@ function MapComponent({onSelect, setProps, total}) {
                         //map.current.setFeatureState( { source: 'states', id: hoveredStateId },{ hover: true } );
                     }
 
-
+*/
                 }
+                lastCell = {contract_id, id: e.features[0].id, x:e.features[0].properties.x, y:e.features[0].properties.y}
                 hoveredCell = e.features[0]
             }
         });
@@ -175,8 +296,13 @@ function MapComponent({onSelect, setProps, total}) {
             zoom:     zoom
         });
         map.current.on('load',()=>{
-            load('d2F668a8461D6761115dAF8Aeb3cDf5F40C532C6')
-            load('2953399124f0cbb46d2cbacd8a89cf0599974963')
+            for(let i=0; i<9; i++){
+                const x = i%3
+                const y = Math.floor(i/3,0)
+                const contractID = loadedArea[y][x]
+                load(contractID)
+            }
+
 
             
         map.current.on('click', (e) => {
@@ -190,8 +316,6 @@ function MapComponent({onSelect, setProps, total}) {
                 const end_x = startX < endX ? endX : startX
                 const end_y = startY < endY ? endY : startY
                 const {source:contract_id} = selectedCell
-                console.log(selectedCell)
-                console.log(contract_id)
                 const list = []
 
                 for(let x=start_x; x<=end_x; x++)
